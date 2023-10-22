1e080174dc600a6647183d7815ea9d2d1e9faacf
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-10-22 21:59:13 +0900

    chore: fix import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mood_trend_flutter/presentation/app.dart';
+import 'package:mood_trend_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
