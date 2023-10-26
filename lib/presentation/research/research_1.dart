5d46f7e8aa8d2f5eefbb3b28ffd0473502581673
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-10-27 00:10:51 +0900

    feat: 調査用ページ追加（調査完了後にアーカイブする予定）

---
+++ lib/presentation/research/research_1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Research1 extends StatelessWidget {
+  const Research1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mood Trend'),
+      ),
+      body: const Center(
+        child: Text('Research1'),
+      ),
+    );
+  }
+}
