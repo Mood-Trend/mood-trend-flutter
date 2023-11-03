81eb280cc1bfdce5f8579bab66c7725faac1deb1
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-11-03 16:38:38 +0900

    feat: 気分値リポジトリ実装

--- lib/infrastructure/firebase/mood_point_repository.dart
+++ lib/infrastructure/firebase/mood_point_repository.dart
@@ -1,4 +1,122 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../domain/app_exception.dart';
+import 'auth_repository.dart';
+import 'firebase_provider.dart';
+
+/// [MoodPointRepository] のインスタンスを提供する [Provider]
+final moodPointRepositoryProvider = Provider<MoodPointRepository>(
+  (ref) => MoodPointRepository(
+    moodPointsCollectionRef: ref.read(moodPointsCollectionRefProvider),
+  ),
+);
+
+/// MoodPoints コレクションの参照結果を提供する [Provider].
+final moodPointsCollectionRefProvider = Provider(
+  (ref) => ref
+      .watch(firebaseFirestoreProvider)
+      .collection('users')
+      .doc(ref.read(userIdProvider)!)
+      .collection('mood_points')
+      .withConverter<MoodPointDocument>(
+        fromFirestore: (snapshot, _) => MoodPointDocument.fromJson(
+          snapshot.id,
+          snapshot.data()!,
+        ),
+        toFirestore: (confDoc, options) => confDoc.toJson(),
+      ),
+);
+
+/// MoodPoints コレクションのドキュメントを操作する Repository
+class MoodPointRepository {
+  MoodPointRepository({
+    required this.moodPointsCollectionRef,
+  });
+
+  final CollectionReference<MoodPointDocument> moodPointsCollectionRef;
+
+  /// MoodPoint ドキュメントを追加する。
+  Future<void> add({
+    required int point,
+    required int plannedVolume,
+    required DateTime moodDate,
+  }) {
+    try {
+      return moodPointsCollectionRef.add(
+        MoodPointDocument(
+          pointId: '',
+          point: point,
+          plannedVolume: plannedVolume,
+          moodDate: moodDate,
+        ),
+      );
+    } on FirebaseException catch (e) {
+      throw AppException('Firestore の追加処理でエラーが発生しました: ${e.code}');
+    } catch (e) {
+      throw AppException('予期しないエラーが発生しました: $e');
+    }
+  }
+
+  /// MoodPoint ドキュメントを更新する。
+  Future<void> update({
+    required String pointId,
+    required int point,
+    required int plannedVolume,
+    required DateTime moodDate,
+  }) async {
+    final moodPointDoc = MoodPointDocument(
+      pointId: pointId,
+      point: point,
+      plannedVolume: plannedVolume,
+      moodDate: moodDate,
+    );
+    try {
+      await moodPointsCollectionRef
+          .doc(moodPointDoc.pointId)
+          .update(moodPointDoc.toJson());
+    } on FirebaseException catch (e) {
+      throw AppException('Firestore の更新処理でエラーが発生しました: ${e.code}');
+    } catch (e) {
+      throw AppException('予期しないエラーが発生しました: $e');
+    }
+  }
+
+  /// MoodPoint のドキュメントを削除する。
+  Future<void> delete({required String pointId}) async {
+    try {
+      await moodPointsCollectionRef.doc(pointId).delete();
+    } on FirebaseException catch (e) {
+      throw AppException('Firestore の削除処理でエラーが発生しました: ${e.code}');
+    } catch (e) {
+      throw AppException('予期しないエラーが発生しました: $e');
+    }
+  }
+
+  /// MoodPoint のドキュメントを取得する。
+  Future<MoodPointDocument?> get({required String pointId}) async {
+    try {
+      final doc = await moodPointsCollectionRef.doc(pointId).get();
+      return doc.data();
+    } on FirebaseException catch (e) {
+      throw AppException('Firestore の取得処理でエラーが発生しました: ${e.code}');
+    } catch (e) {
+      throw AppException('予期しないエラーが発生しました: $e');
+    }
+  }
+
+  /// MoodPoint のドキュメントを全取得する。
+  Future<List<MoodPointDocument>> getAll() async {
+    try {
+      final querySnapshot = await moodPointsCollectionRef.get();
+      return querySnapshot.docs.map((doc) => doc.data()).toList();
+    } on FirebaseException catch (e) {
+      throw AppException('Firestore の取得処理でエラーが発生しました: ${e.code}');
+    } catch (e) {
+      throw AppException('予期しないエラーが発生しました: $e');
+    }
+  }
+}
 
 /// Firebase Firestore に保存される気分数値（予定数含む）のドキュメントモデル
 class MoodPointDocument {
