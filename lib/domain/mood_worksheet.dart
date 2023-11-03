4bb7529ebe9ef8a35a69703a97262588bd81ac6f
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-11-03 15:51:43 +0900

    feat: 症状ワークシートのエンティティを定義

---
+++ lib/domain/mood_worksheet.dart
@@ -0,0 +1,42 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'mood_worksheet.freezed.dart';
+
+/// 症状ワークシート
+@freezed
+class MoodWorksheet with _$MoodWorksheet {
+  const factory MoodWorksheet({
+    /// ワークシート ID
+    required String worksheetId,
+
+    /// -5
+    required String minus_5,
+
+    /// -4
+    required String minus_4,
+
+    /// -3
+    required String minus_3,
+
+    /// -2
+    required String minus_2,
+
+    /// -1
+    required String minus_1,
+
+    /// +1
+    required String plus_1,
+
+    /// +2
+    required String plus_2,
+
+    /// +3
+    required String plus_3,
+
+    /// +4
+    required String plus_4,
+
+    /// +5
+    required String plus_5,
+  }) = _MoodWorksheet;
+}
