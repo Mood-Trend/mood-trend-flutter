723e9533ff145ba85d8275b1d4fd55f82eb33b3e
SatoruInoue2003 <satoru.inoue2003@gmail.com>
2023-11-16 21:05:46 +0900

    feat: 表示期間の切り替えが出来る表の実装

--- lib/presentation/research/research_2.dart
+++ lib/presentation/research/research_2.dart
@@ -1,17 +1,110 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Research2 extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
+
+class Research2 extends ConsumerStatefulWidget {
   const Research2({super.key});
 
   @override
+  Research2State createState() => Research2State();
+}
+
+class Research2State extends ConsumerState<Research2> {
+  DateTime visibleMinimum = DateTime(2023, 11, 1);
+  DateTime visibleMaximum = DateTime(2023, 11, 30);
+  final moodDataList = <MoodData>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    moodDataList.addAll(List<MoodData>.generate(
+      DateTime(2023, 12, 31).difference(DateTime(2023, 10, 1)).inDays + 1,
+      (index) {
+        DateTime date = DateTime(2023, 10, 1).add(Duration(days: index));
+
+        int points;
+        do {
+          points = Random().nextInt(11) - 5;
+        } while (points == 0);
+
+        return MoodData(date, points.toDouble());
+      },
+    ));
+  }
+
+  void updateVisibleRange(DateTime start, DateTime end) {
+    setState(() {
+      visibleMinimum = start;
+      visibleMaximum = end;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    TooltipBehavior tooltipBehavior = TooltipBehavior(enable: true);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mood Trend'),
+        actions: <Widget>[
+          DropdownButton<int>(
+            value: visibleMinimum.month,
+            items: List.generate(12, (index) {
+              int month = index + 1;
+              return DropdownMenuItem(
+                value: month,
+                child: Text('2023年 $month月'),
+              );
+            }),
+            onChanged: (int? month) {
+              if (month != null) {
+                DateTime start = DateTime(2023, month);
+                DateTime end =
+                    DateTime(2023, month + 1).subtract(const Duration(days: 0));
+                updateVisibleRange(start, end);
+              }
+            },
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Research2'),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.95,
+          heightFactor: 0.5,
+          child: SfCartesianChart(
+            primaryXAxis: DateTimeAxis(
+              visibleMinimum: visibleMinimum,
+              visibleMaximum: visibleMaximum,
+            ),
+            primaryYAxis: NumericAxis(minimum: -5, maximum: 5),
+            title: ChartTitle(text: 'Monthly Mood Trend'),
+            legend: const Legend(isVisible: false),
+            tooltipBehavior: tooltipBehavior,
+            series: <LineSeries<MoodData, DateTime>>[
+              LineSeries<MoodData, DateTime>(
+                dataSource: moodDataList,
+                xValueMapper: (MoodData sales, _) => sales.date,
+                yValueMapper: (MoodData sales, _) => sales.points,
+                dataLabelSettings: const DataLabelSettings(isVisible: false),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+String formatDateTime(DateTime dateTime) {
+  return DateFormat('M/d').format(dateTime);
+}
+
+class MoodData {
+  MoodData(this.date, this.points);
+  final DateTime date;
+  final double points;
 }
