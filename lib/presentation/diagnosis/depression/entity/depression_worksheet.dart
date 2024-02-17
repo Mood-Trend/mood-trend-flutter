bea8f7251505adb9b91dfe9dc3a08dea4f75be74
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-17 14:55:51 +0900

    refactor: DepressionWorksheetFactory to accept additional parameters

--- lib/presentation/diagnosis/depression/entity/depression_worksheet.dart
+++ lib/presentation/diagnosis/depression/entity/depression_worksheet.dart
@@ -15,16 +15,29 @@ enum DepressionType {
 /// [DepressionWorksheet] を生成するファクトリクラス
 class DepressionWorksheetFactory {
   /// [DepressionType] に応じた [DepressionWorksheet] を生成する
-  static DepressionWorksheet create(DepressionType type) {
+  static DepressionWorksheet create(
+    DepressionType type, [
+    String minus_1 = '',
+    String minus_2 = '',
+    String minus_3 = '',
+    String minus_4 = '',
+    String minus_5 = '',
+  ]) {
     switch (type) {
       case DepressionType.melancholy:
-        return MelancholyTypeWorksheet();
+        return _MelancholyTypeWorksheet();
       case DepressionType.poorThinking:
-        return PoorThinkingTypeWorksheet();
+        return _PoorThinkingTypeWorksheet();
       case DepressionType.sleepDisorders:
-        return SleepDisordersTypeWorksheet();
+        return _SleepDisordersTypeWorksheet();
       case DepressionType.other:
-        return OtherDepressionTypeWorksheet();
+        return _OtherDepressionTypeWorksheet(
+          minus_1: minus_1,
+          minus_2: minus_2,
+          minus_3: minus_3,
+          minus_4: minus_4,
+          minus_5: minus_5,
+        );
       default:
         throw AppException('Unsupported ManicType: $type');
     }
@@ -41,57 +54,66 @@ abstract class DepressionWorksheet {
 }
 
 /// [DepressionWorksheet] を具象化した憂鬱のワークシート
-class MelancholyTypeWorksheet extends DepressionWorksheet {
+class _MelancholyTypeWorksheet extends DepressionWorksheet {
   @override
-  String get minus_1 => 'どちらかというと後ろ向きである';
+  final String minus_1 = 'どちらかというと後ろ向きである';
   @override
-  String get minus_2 => '軽度の憂鬱感があり、日常の中でやりたくないことが増えている';
+  final String minus_2 = '軽度の憂鬱感があり、日常の中でやりたくないことが増えている';
   @override
-  String get minus_3 => '憂鬱感が強く、生活が厳しい。普段の活動に対する興味をほとんど持てない';
+  final String minus_3 = '憂鬱感が強く、生活が厳しい。普段の活動に対する興味をほとんど持てない';
   @override
-  String get minus_4 => '強い憂鬱感があり、生活に支障が出ている。日常の機能に影響を与えている';
+  final String minus_4 = '強い憂鬱感があり、生活に支障が出ている。日常の機能に影響を与えている';
   @override
-  String get minus_5 => '完全に絶望的で、生きることが苦痛でしかない。日常の動作がままならない';
+  final String minus_5 = '完全に絶望的で、生きることが苦痛でしかない。日常の動作がままならない';
 }
 
 /// [DepressionWorksheet] を具象化した思考力低下のワークシート
-class PoorThinkingTypeWorksheet extends DepressionWorksheet {
+class _PoorThinkingTypeWorksheet extends DepressionWorksheet {
   @override
-  String get minus_1 => 'ものを考えるのが少し億劫に感じる時もある';
+  final String minus_1 = 'ものを考えるのが少し億劫に感じる時もある';
   @override
-  String get minus_2 => '軽度の思考の鈍さがある';
+  final String minus_2 = '軽度の思考の鈍さがある';
   @override
-  String get minus_3 => '思考が鈍く、問題解決や判断力が著しく低下している';
+  final String minus_3 = '思考が鈍く、問題解決や判断力が著しく低下している';
   @override
-  String get minus_4 => '思考が著しく低下しており、集中や記憶に大きな問題がある';
+  final String minus_4 = '思考が著しく低下しており、集中や記憶に大きな問題がある';
   @override
-  String get minus_5 => '思考がまったく機能しておらず、ほとんどのことが理解できない';
+  final String minus_5 = '思考がまったく機能しておらず、ほとんどのことが理解できない';
 }
 
 /// [DepressionWorksheet] を具象化した睡眠障害のワークシート
-class SleepDisordersTypeWorksheet extends DepressionWorksheet {
+class _SleepDisordersTypeWorksheet extends DepressionWorksheet {
   @override
-  String get minus_1 => '寝起きがスッキリしない';
+  final String minus_1 = '寝起きがスッキリしない';
   @override
-  String get minus_2 => '睡眠の質が低く、日中の眠気や疲労感がある';
+  final String minus_2 = '睡眠の質が低く、日中の眠気や疲労感がある';
   @override
-  String get minus_3 => '睡眠が乱れ、十分な休息を得られていない。日中の眠気が強い';
+  final String minus_3 = '睡眠が乱れ、十分な休息を得られていない。日中の眠気が強い';
   @override
-  String get minus_4 => '睡眠が極端に不足、または過度に寝ており、日中の機能が著しく低下している';
+  final String minus_4 = '睡眠が極端に不足、または過度に寝ており、日中の機能が著しく低下している';
   @override
-  String get minus_5 => '眠れずに常に過度の不眠状態で、日中も強い疲労感がある。または日中のほとんどを寝て過ごしている';
+  final String minus_5 = '眠れずに常に過度の不眠状態で、日中も強い疲労感がある。または日中のほとんどを寝て過ごしている';
 }
 
 /// [DepressionWorksheet] を具象化したその他のワークシート
-class OtherDepressionTypeWorksheet extends DepressionWorksheet {
+class _OtherDepressionTypeWorksheet extends DepressionWorksheet {
+  // コンストラクタを追加
+  _OtherDepressionTypeWorksheet({
+    required this.minus_1,
+    required this.minus_2,
+    required this.minus_3,
+    required this.minus_4,
+    required this.minus_5,
+  });
+
   @override
-  String get minus_1 => '';
+  final String minus_1;
   @override
-  String get minus_2 => '';
+  final String minus_2;
   @override
-  String get minus_3 => '';
+  final String minus_3;
   @override
-  String get minus_4 => '';
+  final String minus_4;
   @override
-  String get minus_5 => '';
+  final String minus_5;
 }
