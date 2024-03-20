e2a167bf7987aa016aca7225fbed9280eddbf469
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-03-20 18:42:08 +0900

    style: add comma

--- lib/presentation/graph/input_modal.dart
+++ lib/presentation/graph/input_modal.dart
@@ -24,6 +24,7 @@ class InputModal extends ConsumerStatefulWidget {
 class _MyWidgetState extends ConsumerState<InputModal> with ErrorHandlerMixin {
   double _plannedValue = 0.0;
   DateTime date = DateTime.now();
+  bool _isContinueSaving = false;
 
   int moodNum = 1;
   void _changeSlider(double e) => setState(() {
@@ -39,195 +40,206 @@ class _MyWidgetState extends ConsumerState<InputModal> with ErrorHandlerMixin {
       ),
       height: 410,
       child: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          const SizedBox(
-            height: 16,
-          ),
-          TextButton.icon(
-            onPressed: () async {
-              final selectedDate = await showDatePicker(
-                context: context,
-                initialDate: date,
-                firstDate: DateTime(2000),
-                lastDate: DateTime.now(),
-                builder: (context, child) {
-                  return Theme(
-                    data: Theme.of(context).copyWith(
-                      colorScheme: ColorScheme.light(
-                        primary:
-                            AppColors.green, // 背景色のテーマや選択時の背景色、キャンセルOKボタンの色
-                        onPrimary: AppColors.white, // 選択時のテキストカラー
-                        surface: AppColors.white, // カレンダーの背景色
-                        onSurface: AppColors.black, // カレンダーのテキストカラー
-                        surfaceTint: Colors.transparent, // カレンダーの背景にうっすらかかる色
-                      ),
-                    ),
-                    child: child!,
-                  );
-                },
-              );
-              if (selectedDate != null) {
-                setState(() {
-                  date = selectedDate;
-                });
-              }
-            },
-            icon: Icon(
-              Icons.calendar_month,
-              color: AppColors.green,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 16,
             ),
-            label: Text(
-              DateFormat('yyyy/MM/dd').format(date),
-              style: TextStyle(
-                color: AppColors.black,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(56, 48, 56, 0),
-            child: Slider(
-              value: _moodValue,
-              min: -5.0,
-              max: 5.0,
-              divisions: 10,
-              onChangeStart: (value) {
-                if (value == 0.0) {
-                  setState(() {
-                    _moodValue = _moodValue > 0 ? 1.0 : -1.0;
-                  });
-                }
-              },
-              label: _moodValue.toInt().toString(),
-              onChanged: (value) {
-                if (value != 0.0) {
-                  setState(() {
-                    _moodValue = value;
-                  });
-                }
-              },
-              onChangeEnd: (value) {
-                if (value == 0.0) {
-                  setState(() {
-                    _moodValue = _moodValue > 0 ? 1.0 : -1.0;
-                  });
-                }
-              },
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    S.of(context).moodValue,
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(
-                    width: 75,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-                      child: Center(
-                        child: Text(
-                          _moodValue.toInt().toString(),
-                          style: const TextStyle(fontSize: 52),
+            TextButton.icon(
+              onPressed: () async {
+                final selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: date,
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime.now(),
+                  builder: (context, child) {
+                    return Theme(
+                      data: Theme.of(context).copyWith(
+                        colorScheme: ColorScheme.light(
+                          primary:
+                              AppColors.green, // 背景色のテーマや選択時の背景色、キャンセルOKボタンの色
+                          onPrimary: AppColors.white, // 選択時のテキストカラー
+                          surface: AppColors.white, // カレンダーの背景色
+                          onSurface: AppColors.black, // カレンダーのテキストカラー
+                          surfaceTint: Colors.transparent, // カレンダーの背景にうっすらかかる色
                         ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(56, 24, 56, 0),
-            child: Slider(
-              label: _plannedValue.toInt().toString(),
-              min: 0,
-              max: 16,
-              value: _plannedValue,
-              divisions: 16,
-              onChanged: _changeSlider,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 56, 15, 0),
-                child: IconButton(
-                  onPressed: () {
-                    PageNavigator.push(
-                      context,
-                      TablePage(uid: widget.uid),
+                      child: child!,
                     );
                   },
-                  icon: Icon(
-                    Icons.table_chart_outlined,
-                    color: AppColors.green,
-                  ),
+                );
+                if (selectedDate != null) {
+                  setState(
+                    () {
+                      date = selectedDate;
+                    },
+                  );
+                }
+              },
+              icon: Icon(
+                Icons.calendar_month,
+                color: AppColors.green,
+              ),
+              label: Text(
+                DateFormat('yyyy/MM/dd').format(date),
+                style: TextStyle(
+                  color: AppColors.black,
                 ),
               ),
-              Row(
-                children: [
-                  Text(
-                    S.of(context).plannedVolume,
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(
-                    width: 75,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-                      child: Center(
-                        child: Text(
-                          _plannedValue.toInt().toString(),
-                          style: const TextStyle(fontSize: 52),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(56, 48, 56, 0),
+              child: Slider(
+                value: _moodValue,
+                min: -5.0,
+                max: 5.0,
+                divisions: 10,
+                onChangeStart: (value) {
+                  if (value == 0.0) {
+                    setState(
+                      () {
+                        _moodValue = _moodValue > 0 ? 1.0 : -1.0;
+                      },
+                    );
+                  }
+                },
+                label: _moodValue.toInt().toString(),
+                onChanged: (value) {
+                  if (value != 0.0) {
+                    setState(
+                      () {
+                        _moodValue = value;
+                      },
+                    );
+                  }
+                },
+                onChangeEnd: (value) {
+                  if (value == 0.0) {
+                    setState(
+                      () {
+                        _moodValue = _moodValue > 0 ? 1.0 : -1.0;
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      S.of(context).moodValue,
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 75,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                        child: Center(
+                          child: Text(
+                            _moodValue.toInt().toString(),
+                            style: const TextStyle(fontSize: 52),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(56, 24, 56, 0),
+              child: Slider(
+                label: _plannedValue.toInt().toString(),
+                min: 0,
+                max: 16,
+                value: _plannedValue,
+                divisions: 16,
+                onChanged: _changeSlider,
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 56, 15, 0),
-                child: TextButton(
-                  onPressed: () async {
-                    run(
-                      ref,
-                      action: () async {
-                        // mood_points コレクションにドキュメントを追加
-                        final result = await ref
-                            .read(addMoodPointUsecaseProvider(widget.uid))
-                            .execute(
-                              point: _moodValue.toInt(),
-                              plannedVolume: _plannedValue.toInt(),
-                              moodDate: date,
-                            );
-                        // 同じ日付に既に登録されている場合は上書きされる旨の確認ダイアログを表示
-                        if (!result) {
-                          return _showConfirmDialog(
-                            date: date,
-                            uid: widget.uid,
-                            parent: context,
-                          );
-                        }
-                        Navigator.pop(context);
-                      },
-                      successMessage: S.of(context).inputSuccess,
-                    );
-                  },
-                  child: Text(
-                    S.of(context).inputSave,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 56, 15, 0),
+                  child: IconButton(
+                    onPressed: () {
+                      PageNavigator.push(
+                        context,
+                        TablePage(uid: widget.uid),
+                      );
+                    },
+                    icon: Icon(
+                      Icons.table_chart_outlined,
                       color: AppColors.green,
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ]),
+                Row(
+                  children: [
+                    Text(
+                      S.of(context).plannedVolume,
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 75,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                        child: Center(
+                          child: Text(
+                            _plannedValue.toInt().toString(),
+                            style: const TextStyle(fontSize: 52),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 56, 15, 0),
+                  child: TextButton(
+                    onPressed: () async {
+                      run(
+                        ref,
+                        action: () async {
+                          // mood_points コレクションにドキュメントを追加
+                          final result = await ref
+                              .read(addMoodPointUsecaseProvider(widget.uid))
+                              .execute(
+                                point: _moodValue.toInt(),
+                                plannedVolume: _plannedValue.toInt(),
+                                moodDate: date,
+                              );
+                          // 同じ日付に既に登録されている場合は上書きされる旨の確認ダイアログを表示
+                          if (!result) {
+                            return _showConfirmDialog(
+                              date: date,
+                              uid: widget.uid,
+                              parent: context,
+                            );
+                          }
+                          Navigator.pop(context);
+                        },
+                        successMessage: S.of(context).inputSuccess,
+                      );
+                    },
+                    child: Text(
+                      S.of(context).inputSave,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.green,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
