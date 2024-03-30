f6d6c4e4fbc0f40a49640d09fd6e1de437a940c1
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-03-30 18:10:21 +0900

    feat: Add Stream Provider

---
+++ lib/application/common/states/app_confs_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mood_trend_flutter/infrastructure/firebase/app_confs_repository.dart';
+
+import '../../../domain/app_confs.dart';
+
+/// [AppConfs] を提供する [Provider]
+final appConfsProvider = StreamProvider<AppConfs>(
+  (ref) => ref.watch(appConfsRepositoryProvider).subScribeAppConfs(),
+);
