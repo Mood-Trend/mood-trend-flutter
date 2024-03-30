0a0dfe218d06cdd6caaac902fd2cda2f33dbee23
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-03-30 23:29:41 +0900

    feat: add SavingType enum

--- lib/application/graph/states/is_saving_provider.dart
+++ lib/application/graph/states/is_saving_provider.dart
@@ -1,3 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final isSavingProvider = StateProvider.autoDispose<bool>((ref) => false);
+enum SavingType {
+  /// 保存前
+  none,
+
+  /// 保存中
+  saving,
+
+  /// 保存完了
+  saved,
+
+  /// 保存失敗
+  failed,
+}
+
+final isSavingProvider = StateProvider.autoDispose<SavingType>(
+  (_) => SavingType.none,
+);
