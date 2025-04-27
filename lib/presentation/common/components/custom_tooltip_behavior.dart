d0193b9fb0ab25754bcca46adab127c93a40b3ea
yamamomo <violetraisondetre@gmail.com>
2025-04-27 11:26:41 +0900

    feat:tooltipの実装をcustom_tooltip_behavior.dartへ移行

---
+++ lib/presentation/common/components/custom_tooltip_behavior.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mood_trend_flutter/domain/mood_point.dart';
+import 'package:mood_trend_flutter/generated/l10n.dart';
+import 'package:mood_trend_flutter/presentation/common/theme/app_text_styles.dart';
+import 'package:mood_trend_flutter/utils/app_colors.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+TooltipBehavior createCustomTooltipBehavior(BuildContext context) {
+  return TooltipBehavior(
+    enable: true,
+    canShowMarker: false,
+    color: AppColors.green,
+    textStyle: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: AppColors.white,
+    ),
+    // builder プロパティを追加
+    builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
+        int seriesIndex) {
+      // data から必要な情報を取得
+      final moodPoint = data as MoodPoint;
+      // 日付をフォーマット
+      final formattedDate =
+          DateFormat('MM/dd', Localizations.localeOf(context).languageCode);
+      // 動的なヘッダー文字列を作成
+      final dynamicHeader = formattedDate.format(moodPoint.moodDate);
+
+      // ツールチップに表示する内容を構築
+      return Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              dynamicHeader,
+              style: AppTextStyles.selectedButtonText,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              '${S.of(context).moodValue}: ${moodPoint.point}',
+              style: AppTextStyles.selectedButtonText,
+            ),
+            Text(
+              '${S.of(context).plannedVolume}: ${moodPoint.plannedVolume}',
+              style: AppTextStyles.selectedButtonText,
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
--- lib/presentation/graph/home_page.dart
+++ lib/presentation/graph/home_page.dart
@@ -7,6 +7,7 @@ import 'package:mood_trend_flutter/presentation/common/components/async_value_ha
 import 'package:mood_trend_flutter/presentation/common/components/loading.dart';
 import 'package:mood_trend_flutter/presentation/common/setting_page.dart';
 import 'package:mood_trend_flutter/presentation/common/theme/app_text_styles.dart';
+import 'package:mood_trend_flutter/presentation/common/components/custom_tooltip_behavior.dart';
 import 'package:mood_trend_flutter/utils/datetime_extension.dart';
 import 'package:mood_trend_flutter/utils/page_navigator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -66,53 +67,6 @@ class HomePage extends ConsumerWidget {
 
     // コーチマーク用のターゲット
     final GlobalKey floatingActionButtonKey = GlobalKey();
-
-    // ツールチップの設定
-    final TooltipBehavior tooltipBehavior = TooltipBehavior(
-      enable: true,
-      canShowMarker: false,
-      color: AppColors.green,
-      textStyle: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-        color: AppColors.white,
-      ),
-      // builder プロパティを追加
-      builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
-          int seriesIndex) {
-        // data から必要な情報を取得
-        final moodPoint = data as MoodPoint;
-        // 日付をフォーマット
-        final formattedDate =
-            DateFormat('MM/dd', Localizations.localeOf(context).languageCode);
-        // 動的なヘッダー文字列を作成
-        final dynamicHeader = formattedDate.format(moodPoint.moodDate);
-
-        // ツールチップに表示する内容を構築
-        return Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                dynamicHeader,
-                style: AppTextStyles.selectedButtonText,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                '${S.of(context).moodValue}: ${moodPoint.point}',
-                style: AppTextStyles.selectedButtonText,
-              ),
-              Text(
-                '${S.of(context).plannedVolume}: ${moodPoint.plannedVolume}',
-                style: AppTextStyles.selectedButtonText,
-              ),
-            ],
-          ),
-        );
-      },
-    );
 
     // コーチマークのセットアップ
     Future<void> showCoachMark() async {
@@ -177,6 +131,10 @@ class HomePage extends ConsumerWidget {
             selectedTerm == term ? AppColors.green : Colors.transparent,
       );
     }
+
+    // ツールチップの設定
+    final TooltipBehavior tooltipBehavior =
+        createCustomTooltipBehavior(context);
 
     return Scaffold(
       backgroundColor: AppColors.white,
