c043332300652c76bb1c4c904a6ef06d6aa276b0
IkemotoNanako <chibencaizi@gmail.com>
2025-05-24 11:10:29 +0900

    refactor: print削除

--- lib/presentation/graph/home_page.dart
+++ lib/presentation/graph/home_page.dart
@@ -185,7 +185,6 @@ class HomePage extends ConsumerWidget {
             value: ref.watch(subscribeMoodPointsProvider(userId)),
             loading: () => const OverlayLoading(),
             builder: (moodPoints) {
-              print(moodPoints); // 動作確認用コード
               return Center(
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
