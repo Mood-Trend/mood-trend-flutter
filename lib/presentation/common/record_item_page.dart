b0cc54cf5a79cbca87a96e902546628c1acfe7c8
yamamomo <violetraisondetre@gmail.com>
2025-05-25 10:22:17 +0900

    chore: Providerによる状態管理を一旦排除した

--- lib/presentation/common/record_item_page.dart
+++ lib/presentation/common/record_item_page.dart
@@ -2,10 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mood_trend_flutter/application/common/states/app_confs_provider.dart';
 import 'package:mood_trend_flutter/generated/l10n.dart';
-import 'package:mood_trend_flutter/presentation/common/components/async_value_handler.dart';
-import 'package:mood_trend_flutter/presentation/common/components/loading.dart';
 import 'package:mood_trend_flutter/presentation/common/error_handler_mixin.dart';
 import 'package:mood_trend_flutter/utils/app_colors.dart';
 import 'package:mood_trend_flutter/presentation/common/components/buttons.dart';
@@ -48,7 +45,7 @@ class RecordItemPage extends ConsumerWidget with ErrorHandlerMixin {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final asyncValue = ref.watch(appConfsProvider);
+    // TODO: あたらたProviderを作成して、アプリ設定を取得する
     return Scaffold(
       backgroundColor: AppColors.lightGrey,
       appBar: AppBar(
@@ -57,46 +54,36 @@ class RecordItemPage extends ConsumerWidget with ErrorHandlerMixin {
         centerTitle: true,
         title: Text(S.of(context).settingRecordItem),
       ),
-      body: AsyncValueHandler(
-        value: asyncValue,
-        builder: (appConfs) {
-          return Scaffold(
-            backgroundColor: AppColors.lightGrey,
-            body: ListView(
-              padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-              children: [
-                const SizedBox(height: 28),
-                for (int i = 0; i < recordItems.length; i++) ...[
-                  AppButtons.outlined(
-                    onPressed: () {},
-                    isSelected: recordItems[i].selected,
-                    fixedSize: const Size(double.infinity, 100),
-                    child: Text(
-                      recordItems[i].type.label,
-                      style: AppTextStyles.body.copyWith(fontSize: 22),
-                    ),
-                  ),
-                  if (i != recordItems.length - 1) const SizedBox(height: 28),
-                ],
-              ],
-            ),
-            bottomNavigationBar: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
-              child: SizedBox(
-                width: double.infinity,
-                height: 64,
-                child: AppButtons.secondary(
-                  onPressed: () {},
-                  isSelected: true,
-                  child: Text(S.of(context).inputSave,
-                      style: AppTextStyles.buttonText),
-                ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+        children: [
+          const SizedBox(height: 28),
+          for (int i = 0; i < recordItems.length; i++) ...[
+            AppButtons.outlined(
+              onPressed: () {},
+              isSelected: recordItems[i].selected,
+              fixedSize: const Size(double.infinity, 100),
+              child: Text(
+                recordItems[i].type.label,
+                style: AppTextStyles.body.copyWith(fontSize: 22),
               ),
             ),
-          );
-        },
-        loading: () => const OverlayLoading(),
-        error: (e, s) => const SizedBox(),
+            if (i != recordItems.length - 1) const SizedBox(height: 28),
+          ],
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
+        child: SizedBox(
+          width: double.infinity,
+          height: 64,
+          child: AppButtons.secondary(
+            onPressed: () {},
+            isSelected: true,
+            child:
+                Text(S.of(context).inputSave, style: AppTextStyles.buttonText),
+          ),
+        ),
       ),
     );
   }
