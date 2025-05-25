4b76cd41eb32f0664972deafffdddd3547173a1a
yamamomo <violetraisondetre@gmail.com>
2025-05-25 16:59:23 +0900

    feat: RecordItemType と RecordItem のコメントを追加して可読性を向上させた

--- lib/domain/models/record_item.dart
+++ lib/domain/models/record_item.dart
@@ -4,8 +4,11 @@ import 'record_item_type.dart';
 part 'record_item.freezed.dart';
 part 'record_item.g.dart';
 
+/// [RecordItem] は、記録する項目のモデルクラス
 @freezed
 abstract class RecordItem with _$RecordItem {
+  /// [type]: 記録項目の種類
+  /// [selected]: 選択状態（デフォルトはfalse）
   const factory RecordItem({
     required RecordItemType type,
     @Default(false) bool selected,
