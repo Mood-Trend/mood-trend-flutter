c2de44f65ad806f3c50877154bc296280b8dd529
yamamomo <violetraisondetre@gmail.com>
2025-05-25 16:28:38 +0900

    feat: RecordItemPage で状態管理を活用するようにリファクタリングした。

--- lib/presentation/common/record_item_page.dart
+++ lib/presentation/common/record_item_page.dart
@@ -2,50 +2,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mood_trend_flutter/application/record_item/states/record_item_notifier.dart';
 import 'package:mood_trend_flutter/generated/l10n.dart';
 import 'package:mood_trend_flutter/presentation/common/error_handler_mixin.dart';
 import 'package:mood_trend_flutter/utils/app_colors.dart';
 import 'package:mood_trend_flutter/presentation/common/components/buttons.dart';
 import 'package:mood_trend_flutter/presentation/common/theme/app_text_styles.dart';
 
-enum RecordItemType {
-  sleep("睡眠時間"),
-  steps("歩数"),
-  weather("天気"),
-  memo("一言メモ");
-
-  const RecordItemType(this.label);
-
-  final String label;
-
-  static final Map<String, RecordItemType> _map = {
-    for (final item in RecordItemType.values) item.label: item
-  };
-
-  static RecordItemType fromLabel(String value) {
-    return _map[value] ?? RecordItemType.sleep;
-  }
-}
-
-class RecordItem {
-  final RecordItemType type;
-  bool selected;
-  RecordItem({required this.type, this.selected = false});
-}
-
-final List<RecordItem> recordItems = [
-  RecordItem(type: RecordItemType.sleep, selected: true),
-  RecordItem(type: RecordItemType.steps, selected: false),
-  RecordItem(type: RecordItemType.weather, selected: true),
-  RecordItem(type: RecordItemType.memo, selected: false),
-];
-
 class RecordItemPage extends ConsumerWidget with ErrorHandlerMixin {
   const RecordItemPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: あたらたProviderを作成して、アプリ設定を取得する
+    final recordItems = ref.watch(recordItemNotifierProvider);
+    final notifier = ref.read(recordItemNotifierProvider.notifier);
+
     return Scaffold(
       backgroundColor: AppColors.lightGrey,
       appBar: AppBar(
@@ -60,7 +31,7 @@ class RecordItemPage extends ConsumerWidget with ErrorHandlerMixin {
           const SizedBox(height: 28),
           for (int i = 0; i < recordItems.length; i++) ...[
             AppButtons.outlined(
-              onPressed: () {},
+              onPressed: () => notifier.toggleSelection(i),
               isSelected: recordItems[i].selected,
               fixedSize: const Size(double.infinity, 100),
               child: Text(
